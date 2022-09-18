ac22a5e5ebfd82dc8dac7094d2300a37db4abbaf
Ma-YiHsuan <kaya_dev@icloud.com>
2022-09-18 20:39:23 +0800

    微調捲動頁面時的圖片移動

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 const picRoll = document.querySelector('#picRoll');
+const picTop = picRoll.getBoundingClientRect().top - 250;
 let scrollPos = 0;
 
-window.addEventListener('scroll', function () {
-	if (document.body.getBoundingClientRect().top < scrollPos) {
-		picRoll.classList.add('upSytle');
-		picRoll.classList.remove('downStyle');
-	} else {
-		picRoll.classList.add('downStyle');
-		picRoll.classList.remove('upSytle');
-	}
+function scrollPic() {
+	if (window.scrollY > picTop) {
+		if (document.body.getBoundingClientRect().top < scrollPos) {
+			picRoll.classList.add('upSytle');
+			picRoll.classList.remove('downStyle');
+		} else {
+			picRoll.classList.add('downStyle');
+			picRoll.classList.remove('upSytle');
+		}
 
-	scrollPos = document.body.getBoundingClientRect().top;
-});
+		scrollPos = document.body.getBoundingClientRect().top;
+	}
+}
+
+window.addEventListener('scroll', scrollPic);
