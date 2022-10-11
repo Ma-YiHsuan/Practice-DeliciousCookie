942080f37553e4394c882c8fcfaa914dd667d57b
Ma-YiHsuan <kaya_dev@icloud.com>
2022-10-11 21:08:41 +0800

    使用Vue以及tween.js改寫捲動頁面同時，圖片上下移動。

--- js/app.js
+++ js/app.js
@@ -1,19 +1,50 @@
-const picRoll = document.querySelector('#picRoll');
-const picTop = picRoll.getBoundingClientRect().top - 250;
-let scrollPos = 0;
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.40/vue.esm-browser.js';
 
-function scrollPic() {
-	if (window.scrollY > picTop) {
-		if (document.body.getBoundingClientRect().top < scrollPos) {
-			picRoll.classList.add('upSytle');
-			picRoll.classList.remove('downStyle');
-		} else {
-			picRoll.classList.add('downStyle');
-			picRoll.classList.remove('upSytle');
-		}
+const app = createApp({
+	data() {
+		return {
+			scrollPos: 0,
+			startPoint: 895,
+			translateY: -150,
+		};
+	},
+	watch: {
+		scrollPos(newValue, oldValue) {
+			const newP = newValue / 10;
+			const oldP = oldValue / 10;
+			this.tween(oldP.toFixed(2), newP.toFixed(2));
+		},
+	},
+	mounted() {
+		window.addEventListener('scroll', this.scrollHandler);
+	},
+	methods: {
+		scrollHandler() {
+			this.scrollPos = document.body.getBoundingClientRect().top;
+			this.startPoint = this.$refs.scrollTarget.getBoundingClientRect().top;
+		},
+		tween(start, end) {
+			const vm = this;
+			function animate() {
+				requestAnimationFrame(animate);
+				TWEEN.update();
+			}
+			requestAnimationFrame(animate);
 
-		scrollPos = document.body.getBoundingClientRect().top;
-	}
-}
+			new TWEEN.Tween({ y: start })
+				.to({ y: end }, 1)
+				.onUpdate(function (obj) {
+					if (vm.startPoint < 0) {
+						if (obj.y < -500) {
+							vm.translateY = '-500';
+						} else {
+							vm.translateY = obj.y.toFixed(2);
+						}
+					}
+				})
+				.start();
+		},
+	},
+});
 
-window.addEventListener('scroll', scrollPic);
+app.mount('#app');
